d95210a7e270e48df2b0c43f98a0a4a986526dc5
GregoryBrinkman <GregoryLucasBrinkman@gmail.com>
2017-09-12 01:24:12 -0500

    Parses XML for local weatherfeed

---
+++ weather.js
@@ -0,0 +1,32 @@
+var fetch = require('node-fetch');
+
+fetch("http://w1.weather.gov/xml/current_obs/display.php?stid=KMDH").then((res) => {
+    res.text().then((response) => {
+
+        // 	<location>
+        // 	<station_id>
+        // 	<latitude>
+        // 	<longitude>
+        // 	<observation_time>
+        // 	<weather>
+        // 	<temperature_string>
+        // 	<temp_f>
+        // 	<temp_c>
+        // 	<relative_humidity>
+        // 	<wind_string>
+        // 	<wind_dir>
+        // 	<wind_degrees>
+        // 	<wind_mph>
+        // 	<wind_kt>
+        // 	<pressure_string>
+        // 	<pressure_mb>
+        // 	<pressure_in>
+        // 	<dewpoint_string>
+        // 	<dewpoint_f>
+        // 	<dewpoint_c>
+
+        var temp_f = response.match(/<temp_f[^>]*>([^<]+)<\/temp_f>/)[1];
+        var weather = response.match(/<weather[^>]*>([^<]+)<\/weather>/)[1];
+        console.log(temp_f, weather);
+    });
+});
